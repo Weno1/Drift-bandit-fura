d69ee65a4764bd735559a74ef79550e5d220e49a
Weno1 <crazymati150@gmail.com>
2025-11-21 21:46:07 +0100

    Refactor config.hpp for improved readability and error handling in device selection

--- include/config.hpp
+++ include/config.hpp
@@ -16,16 +16,16 @@ WIRELESS CONNECTION CONFIG
 
 *****************************/
 
-#define PILOT_AP_SSID                   "Drift-Bandit-Fura"
-#define PILOT_AP_PASSWD                 "N6znW5<x@D,0ki(tkWR£'4_!&0TK{(w'[4p1s8A+pBID.g9CpL"
+#define PILOT_AP_SSID               "Drift-Bandit-Fura"
+#define PILOT_AP_PASSWD             "N6znW5<x@D,0ki(tkWR£'4_!&0TK{(w'[4p1s8A+pBID.g9CpL" //Random generated password, you can set your own.
 
-#define PILOT_LISTEN_PORT               3404
+#define PILOT_LISTEN_PORT           3404
 
-#define PILOT_IP                        "192.168.4.1"
-#define CAR_IP                          "192.168.4.2"
+#define PILOT_IP                    "192.168.4.1"
+#define CAR_IP                      "192.168.4.2"
 
-#define NETMASK                         "255.255.255.0"
-#define GATEWAY                         PILOT_IP
+#define NETMASK                     "255.255.255.0"
+#define GATEWAY                     PILOT_IP
 
 
 
@@ -35,12 +35,8 @@ DO NOT EDIT
 FUNCTIONS
 *****************************/
 
-#if defined(PILOT) && defined(CAR)
-#error "!!! Can not be PILOT and CAR at the same time !!!"
-#endif
-
-#if !defined(PILOT) && !defined(CAR)
-#error "!!! Can not be both undefined at the same time !!!"
+#if (defined(PILOT) && defined(CAR)) || (!defined(PILOT) && !defined(CAR))
+#error "Define exactly ONE of PILOT or CAR"
 #endif
 
 #endif /* CONFIG_HPP */
